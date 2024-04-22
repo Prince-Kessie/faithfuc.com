b0120660035ed6e0908884692520f590e48367c0
Prince-Kessie <139052079+Prince-Kessie@users.noreply.github.com>
2024-04-22 15:45:28 -0700

    faithfuc.com

---
+++ about.php
@@ -0,0 +1,340 @@
+<?php include 'includes\header.php'; ?>
+
+<section class="ftco-intro ftco-no-pb img" style="background-image: url(images/bg_3.jpg);">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-10 text-center heading-section heading-section-white ftco-animate">
+					<h2 style="text-align: center; text-decoration: underline; color: #8B0000; ">ACADEMIC PROGRAMS</h2>
+					<h2 class="mb-0" style="color: black;" >Diploma/Certificate/Associate Degree/ B.sc/ BBA</h2>
+					<h2 class="mb-0">100% Approved And Accredited By The National Commission On Higher Education.</h2>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<section class="ftco-section ftco-about ftco-no-pt ftco-no-pb ftco-counter" id="section-counter">
+  <div class="container consult-wrap">
+    <div class="row d-flex align-items-stretch">
+      <div class="col-md-6 wrap-about align-items-stretch d-flex">
+        <div class="img" style="background-image: url(images/about.jpg);"></div>
+      </div>
+      <div class="col-md-6 wrap-about ftco-animate py-md-5 pl-md-5">
+        <div class="heading-section mb-4">
+          <span class="subheading">Welcome to FUC</span>
+          <h2>Who we are really!</h2>
+        </div>
+        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there
+          live the blind texts.</p>
+        <div class="tabulation-2 mt-4">
+          <ul class="nav nav-pills nav-fill d-md-flex d-block">
+            <li class="nav-item">
+              <a class="nav-link active py-2" data-toggle="tab" href="#home1"><span class="ion-ios-home mr-2"></span>
+                Our Vision</a>
+            </li>
+            <li class="nav-item px-lg-2">
+              <a class="nav-link py-2" data-toggle="tab" href="#home2"><span class="ion-ios-person mr-2"></span> Our
+                Mission</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link py-2" data-toggle="tab" href="#home3"><span class="ion-ios-mail mr-2"></span> Our
+                Value</a>
+            </li>
+          </ul>
+          <div class="tab-content bg-light rounded mt-2">
+            <div class="tab-pane container p-0 active" id="home1">
+              <p>Far far away, behind the word mountains, far from the countries Vokalia and
+                Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right
+                at the coast of the Semantics, a large language ocean.</p>
+            </div>
+            <div class="tab-pane container p-0 fade" id="home2">
+              <p>Far far away, behind the word mountains, far from the countries Vokalia and
+                Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right
+                at the coast of the Semantics, a large language ocean.</p>
+            </div>
+            <div class="tab-pane container p-0 fade" id="home3">
+              <p>Far far away, behind the word mountains, far from the countries Vokalia and
+                Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right
+                at the coast of the Semantics, a large language ocean.</p>
+            </div>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="icon"><span class="flaticon-doctor"></span></div>
+              <div class="text">
+                <strong class="number" data-number="1387">0</strong>
+                <span>Happy Clients</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="icon"><span class="flaticon-doctor"></span></div>
+              <div class="text">
+                <strong class="number" data-number="310">0</strong>
+                <span>Success Reports</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="icon"><span class="flaticon-doctor"></span></div>
+              <div class="text">
+                <strong class="number" data-number="35">0</strong>
+                <span>Experienced</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="ftco-section ftco-consult ftco-no-pt ftco-no-pb" style="background-image: url(images/bg_5.jpg);"
+  data-stellar-background-ratio="0.5">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row justify-content-start">
+      <div class="col-md-6 py-5 px-md-5">
+        <div class="py-md-5">
+          <div class="heading-section heading-section-white ftco-animate mb-5">
+            <h2 class="mb-4">Have a question? Ask</h2>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+              blind texts.</p>
+          </div>
+          <form action="#" class="appointment-form ftco-animate">
+            <div class="d-md-flex">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="First Name">
+              </div>
+              <div class="form-group ml-md-4">
+                <input type="text" class="form-control" placeholder="Last Name">
+              </div>
+            </div>
+            <div class="d-md-flex">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Email,  Not compulsory">
+              </div>
+              <div class="form-group ml-md-4">
+                <input type="text" class="form-control" placeholder="Phone">
+              </div>
+            </div>
+            <div class="d-md-flex">
+              <div class="form-group">
+                <textarea name="" id="" cols="30" rows="2" class="form-control" placeholder="Your question"></textarea>
+              </div>
+              <div class="form-group ml-md-4">
+                <input type="submit" value="Send Now" class="btn btn-white py-3 px-4">
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="ftco-section testimony-section">
+  <div class="container-fluid px-md-5">
+    <div class="row justify-content-center mb-5">
+      <div class="col-md-8 text-center heading-section ftco-animate">
+        <span class="subheading">Testimonies</span>
+        <h2 class="mb-4">Our Clients Says</h2>
+        <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary
+          regelialia. It is a paradisematic country</p>
+      </div>
+    </div>
+    <div class="row ftco-animate justify-content-center">
+      <div class="col-md-12">
+        <div class="carousel-testimony owl-carousel">
+          <div class="item">
+            <div class="testimony-wrap d-flex">
+              <div class="user-img" style="background-image: url(images/person_1.jpg)">
+              </div>
+              <div class="text pl-4">
+                <span class="quote d-flex align-items-center justify-content-center">
+                  <i class="icon-quote-left"></i>
+                </span>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live
+                  the blind texts.</p>
+                <p class="name">Racky Henderson</p>
+                <span class="position">Father</span>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="testimony-wrap d-flex">
+              <div class="user-img" style="background-image: url(images/person_2.jpg)">
+              </div>
+              <div class="text pl-4">
+                <span class="quote d-flex align-items-center justify-content-center">
+                  <i class="icon-quote-left"></i>
+                </span>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live
+                  the blind texts.</p>
+                <p class="name">Henry Dee</p>
+                <span class="position">Businesswoman</span>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="testimony-wrap d-flex">
+              <div class="user-img" style="background-image: url(images/person_3.jpg)">
+              </div>
+              <div class="text pl-4">
+                <span class="quote d-flex align-items-center justify-content-center">
+                  <i class="icon-quote-left"></i>
+                </span>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live
+                  the blind texts.</p>
+                <p class="name">Mark Huff</p>
+                <span class="position">Businesswoman</span>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="testimony-wrap d-flex">
+              <div class="user-img" style="background-image: url(images/person_4.jpg)">
+              </div>
+              <div class="text pl-4">
+                <span class="quote d-flex align-items-center justify-content-center">
+                  <i class="icon-quote-left"></i>
+                </span>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live
+                  the blind texts.</p>
+                <p class="name">Rodel Golez</p>
+                <span class="position">Businesswoman</span>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="testimony-wrap d-flex">
+              <div class="user-img" style="background-image: url(images/person_1.jpg)">
+              </div>
+              <div class="text pl-4">
+                <span class="quote d-flex align-items-center justify-content-center">
+                  <i class="icon-quote-left"></i>
+                </span>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live
+                  the blind texts.</p>
+                <p class="name">Ken Bosh</p>
+                <span class="position">Businesswoman</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--Recent blog section-->
+<section class="ftco-section" style="margin-top: -80px;">
+<div class="row justify-content-center mb-5">
+      <div class="col-md-8 text-center heading-section ftco-animate">
+        <span class="subheading">Recent Blogs</span>
+        <h2 class="mb-4">Read our recent blogs</h2>
+        <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary
+          regelialia.</p>
+      </div>
+    </div>
+  <div class="container" style="margin-top: -30px;">
+    <div class="row">
+      <div class="col-md-6 col-lg-4 ftco-animate">
+        <div class="blog-entry">
+          <a href="blog-single.html" class="block-20 d-flex align-items-end"
+            style="background-image: url('images/image_1.jpg');">
+            <div class="meta-date text-center p-2">
+              <span class="day">15</span>
+              <span class="mos">Oct.</span>
+              <span class="yr">2019</span>
+            </div>
+          </a>
+          <div class="text p-4">
+            <h3 class="heading"><a href="#">Finance And Legal Working Streams Occur Throughout</a></h3>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+              blind texts.</p>
+            <div class="d-flex align-items-center mt-4">
+              <p class="mb-0"><a href="#" class="btn btn-primary">Read More <span
+                    class="ion-ios-arrow-round-forward"></span></a></p>
+              <p class="ml-auto mb-0">
+                <a href="#" class="mr-2">Admin</a>
+                <a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 ftco-animate">
+        <div class="blog-entry">
+          <a href="blog-single.html" class="block-20 d-flex align-items-end"
+            style="background-image: url('images/image_2.jpg');">
+            <div class="meta-date text-center p-2">
+              <span class="day">15</span>
+              <span class="mos">Oct.</span>
+              <span class="yr">2019</span>
+            </div>
+          </a>
+          <div class="text p-4">
+            <h3 class="heading"><a href="#">Finance And Legal Working Streams Occur Throughout</a></h3>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+              blind texts.</p>
+            <div class="d-flex align-items-center mt-4">
+              <p class="mb-0"><a href="#" class="btn btn-primary">Read More <span
+                    class="ion-ios-arrow-round-forward"></span></a></p>
+              <p class="ml-auto mb-0">
+                <a href="#" class="mr-2">Admin</a>
+                <a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 ftco-animate">
+        <div class="blog-entry">
+          <a href="blog-single.html" class="block-20 d-flex align-items-end"
+            style="background-image: url('images/image_3.jpg');">
+            <div class="meta-date text-center p-2">
+              <span class="day">15</span>
+              <span class="mos">Oct.</span>
+              <span class="yr">2019</span>
+            </div>
+          </a>
+          <div class="text p-4">
+            <h3 class="heading"><a href="#">Finance And Legal Working Streams Occur Throughout</a></h3>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+              blind texts.</p>
+            <div class="d-flex align-items-center mt-4">
+              <p class="mb-0"><a href="#" class="btn btn-primary">Read More <span
+                    class="ion-ios-arrow-round-forward"></span></a></p>
+              <p class="ml-auto mb-0">
+                <a href="#" class="mr-2">Admin</a>
+                <a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col text-center">
+        <div class="block-27">
+          <ul>
+            <li><a href="#">&lt;</a></li>
+            <li class="active"><span>1</span></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#">&gt;</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php include 'includes\footer.php'; ?>
